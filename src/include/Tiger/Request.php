bd07d6576f335a694768c3bac34916518cd51260
Shea690901 <ginny690901@hotmail.de>
2015-08-15 15:46:09 +0200

    split initialization into constructor and init

--- src/include/Tiger/Request.php
+++ src/include/Tiger/Request.php
@@ -3,9 +3,15 @@ namespace Tiger {
     class Request extends Base {
         private $var_filter = NULL;
 
-        function __construct()
+        public function __construct($init = FALSE)
         {
             parent::__construct();
+            if($init)
+                $thos->init();
+        }
+
+        public function init()
+        {
             if($this->var_filter)
             {
                 $filter = $this->var_filter;
@@ -31,6 +37,18 @@ namespace Tiger {
         public function __destruct()
         {
             parent::__destruct();
+        }
+
+        public function set_var_filter($filter)
+        {
+            if(is_callable($filter))
+                $this->var_filter = $filter;
+            else
+            {
+                if(\DEBUG)
+                    throw new \InvalidArgumentException(sprintf('Invalid Argument to set_var_filter expected callable, got %s', var_export($filter)));
+            }
+
         }
     }
 }
