b90cff149517580ff509b7fec0886fa9ba36bef2
Shea690901 <ginny690901@hotmail.de>
2015-10-16 23:29:17 +0200

    new features
    new functions:
    - new_ally:			enters new alliance into database
    - change_leader:	changes leadership of alliance
    - new_player:		enters new player into database
    - change_ally:		changes ally membership of player
    - add_ally:			gives (new) ally access priviliges,
    					previuos members added as blocked by leader
    
    new features:
    - add_user:			now callable from function which uses transactions
    					itself
    
    - added some comments

--- src/include/GalClash/GCDB.php
+++ src/include/GalClash/GCDB.php
@@ -296,50 +296,86 @@ namespace GalClash {
             return $this->users[$ally];
         }
 
-        public function add_user($ally, $name, $pwd)
+        /*
+        ** general functions to add/change/delete allies without access privileges
+        */
+        public function new_ally($ally)
         {
-            $dbh  = $this->get_handle();
+            if($this->get_ally_id($ally) != -1)
+                throw new Exception('Allianz bereits eingetragen');
+
+            $sth = $this->get_handle()->prepare("INSERT INTO `allianzen` ( `allianz` ) VALUES ( :ally )");
+            $sth->bindValue(":ally", $ally);
+            $sth->execute();
+
+            return $this->get_ally_id($ally);
+        }
+
+        public function change_leader($a_id, $p_id)
+        {
+            $sth = $this->get_handle()->prepare("UPDATE `allianzen` SET `leiter_id` = :p_id WHERE `a_id` = :a_id");
+            $sth->bindValue(":p_id", $p_id, PDO::PARAM_INT);
+            $sth->bindValue(":a_id", $a_id, PDO::PARAM_INT);
+            $sth->execute();
+        }
+
+        /*
+        ** general functions to add/change/delete players without access privileges
+        */
+        public function new_player($name, $a_id)
+        {
+            if($this->get_player_id($name) != -1)
+                throw new Exception('Spieler bereits eingetragen');
+
+            $sth = $this->get_handle()->prepare("INSERT INTO `spieler` ( `name`, `a_id` ) VALUES ( :name, :a_id )");
+            $sth->bindValue(":name", $name);
+            $sth->bindValue(":a_id", $a_id, PDO::PARAM_INT);
+            $sth->execute();
+
+            return $this->get_player_id($name);
+        }
+
+        public function change_ally($p_id, $a_id)
+        {
+            $sth = $this->get_handle()->prepare("UPDATE `spieler` SET `a_id` = :a_id WHERE `s_id` = :p_id");
+            $sth->bindValue(":a_id", $a_id, PDO::PARAM_INT);
+            $sth->bindValue(":p_id", $p_id, PDO::PARAM_INT);
+            $sth->execute();
+        }
+
+        /*
+        ** general functions to add/change/delete allies with access privileges
+        */
+        public function add_ally($ally, $leader, $pwd)
+        {
+            $dbh = $this->get_handle();
 
             $a_id = $this->get_ally_id($ally);
-            $s_id = $this->get_player_id($name);
-            $m_id = $this->get_user_id($name);
+            $p_id = $this->get_player_id($leader);
+            $u_id = $this->get_user_id($leader);
 
-            if($m_id != -1)
-            {
-                throw new Exception("User bereits eingetragen");
-            }
-            if($s_id == -1)
-            {
-                $sth1 = $dbh->prepare("INSERT INTO spieler (name, a_id) VALUES ( :name, :a_id )");
-                $sth2 = $dbh->prepare("INSERT INTO user_pwd ( s_id, pwd ) VALUES ( ( SELECT s_id FROM spieler WHERE name = :name ), :pwd )");
-            }
-            else
-            {
-                $sth1 = $dbh->prepare("UPDATE spieler SET a_id = :a_id WHERE s_id = :s_id");
-                $sth2 = $dbh->prepare("INSERT INTO user_pwd ( s_id, pwd ) VALUES ( :s_id, :pwd )");
-            }
+            $sth1 = $dbh->prepare("INSERT INTO blacklisted ( a_id ) VALUES ( :a_id )");
+            $sth2 = $dbh->prepare("INSERT INTO user_pwd ( s_id ) SELECT s_id FROM spieler " .
+                    "WHERE a_id = :a_id AND NOT EXISTS ( SELECT 1 FROM user_pwd WHERE user_pwd.s_id  = spieler.s_id )");
+
             try {
                 $dbh->beginTransaction();
 
+                if($a_id == -1)
+                    $a_id = $this->new_ally($ally);
+                if($u_id == -1)
+                    $this->add_user($ally, $leader, $pwd, FALSE);
+                $this->change_leader($a_id, $this->get_player_id($leader));
+
                 $sth1->bindValue(":a_id", $a_id, PDO::PARAM_INT);
-                $sth2->bindValue(":pwd", $pwd);
-                if($s_id == -1)
-                {
-                    $sth1->bindValue(":name", $name);
-                    $sth2->bindValue(":name", $name);
-                }
-                else
-                {
-                    $sth1->bindValue(":s_id", $s_id, PDO::PARAM_INT);
-                    $sth2->bindValue(":s_id", $s_id, PDO::PARAM_INT);
-                }
+                $sth2->bindValue(":a_id", $a_id, PDO::PARAM_INT);
                 $sth1->execute();
                 $sth2->execute();
 
                 $dbh->commit();
             }
             catch(Exception $e) {
-                $dbh->rollBack();
+                //$dbh->rollBack();
                 if(\DEBUG)
                 {
                     $ei[0] = $sth1->errorInfo();
@@ -349,6 +385,53 @@ namespace GalClash {
                 else
                     throw new \Tiger\DB_Exception(\Tiger\DB_Exception::DB_EXECUTION_ERROR, sprintf("Fehler bei Datenbankabfrage: '%d'<br />\n", $e->getCode()));
             }
+        }
+
+        /*
+        ** general functions to add/change/delete players with access privileges (user)
+        */
+        public function add_user($ally, $name, $pwd, $trans = TRUE)
+        {
+            $dbh  = $this->get_handle();
+
+            $a_id = $this->get_ally_id($ally);
+            $p_id = $this->get_player_id($name);
+            $m_id = $this->get_user_id($name);
+
+            if($m_id != -1)
+            {
+                throw new Exception("User bereits eingetragen");
+            }
+
+            $sth = $dbh->prepare("INSERT INTO `user_pwd` ( `s_id`, `pwd` ) VALUES ( :p_id, :pwd )");
+            try {
+                if($trans)
+                    $dbh->beginTransaction();
+
+                if($p_id == -1)
+                    $p_id = $this->new_player($name, $a_id);
+                else
+                    $this->change_ally($p_id, $a_id);
+
+                $sth->bindValue(":p_id", $p_id, PDO::PARAM_INT);
+                $sth->bindValue(":pwd", $pwd);
+                $sth->execute();
+
+                if($trans)
+                    $dbh->commit();
+            }
+            catch(Exception $e) {
+                if($trans)
+                    $dbh->rollBack();
+                if(\DEBUG)
+                {
+                    $ei = $sth->errorInfo();
+                    throw new \Tiger\DB_Exception(\Tiger\DB_Exception::DB_EXECUTION_ERROR, sprintf("Fehler bei Datenbankabfrage(%s/%s/%s):\n'%s'", $ei[0], $ei[1], $ei[2], $e->getMessage()));
+                }
+                else
+                    throw new \Tiger\DB_Exception(\Tiger\DB_Exception::DB_EXECUTION_ERROR, sprintf("Fehler bei Datenbankabfrage: '%d'<br />\n", $e->getCode()));
+            }
+            return $this->get_user_id($name);
         }
 
         public function del_user($name)
